72ecc6e36c1e8fabeffcc6fa9fbdf0ae68921a60
백준영 <jupiterbjy@gmail.com>
2020-02-21 14:32:51 +0000

    Temporal Push. Can't work pyautogui without display.

---
+++ MacroMethods.py
@@ -0,0 +1,31 @@
+import cv2
+import sys
+import pyautogui as pgui
+import time
+import numpy as np
+
+
+
+class Dependency:
+    
+    def __init__(self):
+        self.pos1=
+    
+    def ScreenShotArea(pos1, pos2):
+        "Takes a screenshot of area as a file-like object. (probably)"
+        ""
+        pass
+        return pgui.screenshot(region=(pos))
+
+        
+class ImageMacro(Dependency):
+    def __init__(self):
+        pass
+    
+    def base():
+        pass
+
+class Wait:
+    def __init__(self, )
+    
+
---
+++ ui_main.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+
+from pymacro import Ui_MainWindow 
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    OneFile.DetectFrozen()
+    main()
